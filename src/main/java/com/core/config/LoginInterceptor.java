fc31a209c3c519e1272f2fd87c8819dc26f22976
1374559379 <1374559379@qq.com>
2019-07-03 10:12:39 +0800

    日志包切换

--- src/main/java/com/core/config/LoginInterceptor.java
+++ src/main/java/com/core/config/LoginInterceptor.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 
 /**
  * Created by zww on 2019-06-28.
