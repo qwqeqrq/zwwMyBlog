f52ce8bbb6add5d3be6658714a3fe59f17c98be0
1374559379 <1374559379@qq.com>
2019-08-14 11:28:04 +0800

    解决拦截器拦截css问题 添加牛蛙侠页面

--- src/main/java/com/core/config/WebMvcConfig.java
+++ src/main/java/com/core/config/WebMvcConfig.java
@@ -16,6 +16,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //TODO  addPathPatterns方法添加的路径都要进行拦截，excludePathPatterns这是放行的路径不用检测登录状态
-        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/sys/login/**","/Email/unsubscribe/**").excludePathPatterns("/");
+        //2019年8月14日11:00:58 放行文件加入css放行 *.css解决拦截器 导致的页面多次访问后端获取css,css.map
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/**/*.css","/sys/login/**","/Email/unsubscribe/**");
     }
 }
--- src/main/java/com/core/console/controller/UserController.java
+++ src/main/java/com/core/console/controller/UserController.java
@@ -2,10 +2,12 @@ package com.core.console.controller;
 
 import com.alibaba.druid.support.json.JSONUtils;
 import com.core.blog.po.Result;
+import com.core.blog.uitls.StringUtils;
 import com.core.console.po.UserBean;
 import com.core.console.service.UserService;
 import com.core.console.uitl.IpTools;
 import com.core.console.uitl.PageInfo;
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -82,12 +85,19 @@ public class UserController {
      * @修改人和其它信息：
      */
     @RequestMapping(value = "login")
-    public String doLogin(UserBean user, Model model, HttpServletRequest httpServletRequest) {
+    public String doLogin(UserBean user, Model model, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         logger.info(">>>>>>>>>>>>>>>>>>>>>>>访问者ip:" + IpTools.getIpAddress(httpServletRequest));
+        //httpServletResponse.setContentType("text/html;charset=utf-8");
+        Result result = new Result();
         try {
+            if (checkUserCodeisEmpty(user)){
+                logger.info("=================空用户名密码在刷新登录页================");
+                result.setCode(0);
+                model.addAttribute("result", result);
+                return "login";//跳转登录页
+            }
             String passWord = user.getUserPassword();
             user.setUserPassword("");
-            Result result = new Result();
             List<UserBean> userBeanList = userService.getUser(user, null);
             if (userBeanList != null && !userBeanList.isEmpty()) {
                 if (userBeanList.get(0).getUserPassword().equals(passWord)) {
@@ -115,5 +125,17 @@ public class UserController {
             logger.error(JSONUtils.toJSONString(e));
         }
         return "系统维护中！！！敬请谅解";
+    }
+
+    /**
+     * @描述： 检查前端输入的用户名是否为空
+     * @参数： [user]
+     * @返回值： boolean
+     * @创建人： zhangww
+     * @创建时间： 2019-08-14
+     * @修改人和其它信息：
+     */
+    private boolean checkUserCodeisEmpty(UserBean user) {
+        return StringUtils.isEmpty(user.getUserCode());
     }
 }
